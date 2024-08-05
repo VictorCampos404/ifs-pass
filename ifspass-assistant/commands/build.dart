b6cdb5c518181b5af3a2a3c2a4b771fb2c76a200
Victor Campos <campos.victor@dilettasolutions.com>
2024-08-05 19:46:29 -0300

    app commands

--- ifspass-assistant/commands/build.dart
+++ ifspass-assistant/commands/build.dart
@@ -1,17 +1,5 @@
 import 'dart:io';
 
-
-// //
-// // NÃO EDITAR, ARQUIVO GERADO POR CÓDIGO
-// //
-
-// import 'package:system_package/system.dart';
-// import 'package:ifs_pass/apps/new_app/module.dart';
-
-// List<SystemAppModule> get APP_MODULES => [
-//   NewAppModule(),
-// ];
-
 class BuildAppData{
   final String path;
   final String name;
